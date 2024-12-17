22d804e06e59dae40e4cc15e164b6ab7a1f48252
RosaHuisman <rosa.huisman@gmail.com>
2024-12-17 20:19:19 +0100

    progressive web app installation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Importation du Service Worker
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +12,8 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Enregistrement du Service Worker pour activer la PWA
+serviceWorkerRegistration.register(); // Remplacer `unregister()` par `register()`
+
+// Si vous voulez suivre les performances de l'application
 reportWebVitals();
